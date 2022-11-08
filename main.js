94641c6f0e01b37c4e5b2d61c1d6707b33ef7b3a
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-11-08 14:11:56 -0800

    weather app start

---
+++ main.js
@@ -0,0 +1,27 @@
+let weather = {
+  apiKey: "48c35fc9bf8a18960ac191e236105942",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    console.log(name, icon, description, temp, humidity, speed);
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    document.querySelector(".description").innerText = description;
+  },
+};
