7d3b5f8f6d11c1df8802c47c2887780fe2722e93
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-11-08 14:40:48 -0800

    search and parameters working

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let weather = {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&units=metric&appid=" +
+        "&units=imperial&appid=" +
         this.apiKey
     )
       .then((response) => response.json())
@@ -20,8 +20,28 @@ let weather = {
 
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      "https://openweathermap.org/img/wn/" + icon + ".png";
 
     document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + " °F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " Mph";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
